25ac86bdbb00316f3de9567f33b6f1965ff95a0c
Marcin Świstak <m.swistak.n@gmail.com>
2025-04-06 13:49:14 +0200

    17. Dodanie dokumentacji Javadoc do klasy IntLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,10 +1,18 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * Klasa implementuje dynamiczną listę jednokierunkową dla intów,
+ * dostępne metody to: push, isEmpty, isFull, top, pop
+ */
 public class IntLinkedList {
 
     private static final int RETURN_MINUS_ONE = -1;
     private Node last;
 
+    /**
+     * Dodaje podany element i do listy na sam koniec
+     * @param i wartosc do dodania do listy
+     */
     public void push(int i) {
         if (last == null)
             last = new Node(i);
@@ -15,20 +23,36 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     * Zwraca czy lista jest pusta
+     * @return true jezeli pusta, false jeżeli nie
+     */
     public boolean isEmpty() {
         return last == null;
     }
 
+    /**
+     * Zwraca czy lista jest pełna
+     * @return true jeżeli pełna, false jeżeli nie
+     */
     public boolean isFull() {
         return false;
     }
 
+    /**
+     * Bez usuwania zwraca ostatni element listy
+     * @return wartość ostatniego elementu listy albo -1 w przypadku gdy lista jest pusta
+     */
     public int top() {
         if (isEmpty())
             return RETURN_MINUS_ONE;
         return last.getValue();
     }
 
+    /**
+     * Zwraca ostatni element listy z usunięciem go
+     * @return wartosc ostatniego elementu listy albo -1 w przypadku gdy lista jest pusta
+     */
     public int pop() {
         if (isEmpty())
             return RETURN_MINUS_ONE;
@@ -37,6 +61,9 @@ public class IntLinkedList {
         return ret;
     }
 
+    /**
+     * Klasa wewnętrzna przedstawiająca pojedynczy element listy
+     */
     private static class Node {
 
         private final int value;
